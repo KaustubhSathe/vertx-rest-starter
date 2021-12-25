668649701108b121e17022ee0a4ac1514ffd0299
KaustubhSathe <f2015466p@alumni.bits-pilani.ac.in>
2021-12-26 03:21:29 +0530

    change

---
+++ src/main/java/com/vertx/starter/AbstractRestVerticle.java
@@ -0,0 +1,44 @@
+package com.vertx.starter;
+
+import io.reactivex.rxjava3.core.Completable;
+import io.vertx.core.Context;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.rxjava3.core.AbstractVerticle;
+import io.vertx.rxjava3.core.http.HttpServer;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractRestVerticle extends AbstractVerticle {
+  private final String packageName;
+  private HttpServer httpServer;
+
+  public AbstractRestVerticle(String packageName){
+    this.packageName = packageName;
+  }
+
+  @Override
+  public Completable rxStart() {
+
+  }
+
+  @Override
+  public Completable rxStop() {
+    return super.rxStop();
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+}

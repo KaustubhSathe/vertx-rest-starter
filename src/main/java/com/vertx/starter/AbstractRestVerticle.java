9b03365516613b26a2660a2405171c3498e5d758
kaustubhsathefc <89383411+kaustubhsathefc@users.noreply.github.com>
2021-12-26 21:48:44 +0530

    change

--- src/main/java/com/vertx/starter/AbstractRestVerticle.java
+++ src/main/java/com/vertx/starter/AbstractRestVerticle.java
@@ -1,9 +1,11 @@
 package com.vertx.starter;
 
 import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
 import io.vertx.core.Context;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServerOptions;
 import io.vertx.rxjava3.core.AbstractVerticle;
 import io.vertx.rxjava3.core.http.HttpServer;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 public abstract class AbstractRestVerticle extends AbstractVerticle {
   private final String packageName;
   private HttpServer httpServer;
+  private HttpConfig
 
   public AbstractRestVerticle(String packageName){
     this.packageName = packageName;
@@ -20,6 +23,11 @@ public abstract class AbstractRestVerticle extends AbstractVerticle {
   @Override
   public Completable rxStart() {
 
+  }
+
+  private Single<HttpServer> startHttpServer() {
+    HttpServerOptions options = new HttpServerOptions()
+      .setHost(htt)
   }
 
   @Override
